64e0d90adc0957415ace2bc910b236f4b1971a29
JunYeong Park <101688752+farmJun@users.noreply.github.com>
2022-05-27 22:22:34 +0900

    Graph
    Graph

---
+++ Graph/W13_P2.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int n,m,k,f;
+int graph[501][501];
+
+int main(){
+    cin>>n>>m;
+
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cin>>graph[i][j];
+        }
+    }
+
+    for(int i=0;i<m;i++){
+        cin>>k>>f;
+
+        vector<int> groupF;
+        vector<int> groupP;
+        vector<int> groupA;
+
+        for(int j=1;j<=n;j++){
+            if(k==j){
+                continue;
+            }
+            if(graph[k][j]==2){
+                groupP.push_back(j);
+            }
+            else if(graph[k][j]==1){
+                groupF.push_back(j);
+            }
+            else if(graph[k][j]==0){
+                groupA.push_back(j);
+            }
+        }
+
+        int result=0;
+        int result1=0;
+        if(f==0){
+            for(int b=0;b<groupA.size();b++){
+                for(int a=0;a<groupF.size();a++) {
+                    if (graph[groupF[a]][groupA[b]] == 1) {
+                        result1 = 1;
+                        result = 1;
+                    }
+                }
+                if(result1==1){
+                    cout << groupA[b] << " ";
+                }
+                result1=0;
+            }
+            if(result==0){
+                cout<<"None\n";
+            }
+            else{
+                cout<<"\n";
+            }
+        }
+        else if(f==1){
+            for(int b=0;b<groupA.size();b++){
+                for(int a=0;a<groupF.size();a++) {
+                    if (graph[groupF[a]][groupA[b]] == 2) {
+                        result1 = 1;
+                        result = 1;
+                    }
+                }
+                if(result1==1){
+                    cout << groupA[b] << " ";
+                }
+                result1=0;
+            }
+            if(result==0){
+                cout<<"0\n";
+            }
+            else{
+                cout<<"\n";
+            }
+        }
+
+    }
+    return 0;
+}
